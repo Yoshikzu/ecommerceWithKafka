f3bbdac5b36c210dedaf53259a9be1d9d80a4371
yoshikzu <marcelo_yoshikazu@hotmail.com>
2023-09-04 20:35:27 -0300

    Criação de duas classes de consumidores e criação das keys nas classes produtoras

--- src/main/java/br/com/yoshikazu/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/yoshikazu/ecommerce/NewOrderMain.java
@@ -1,11 +1,13 @@
 package br.com.yoshikazu.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import static br.com.yoshikazu.ecommerce.NewOrderMain.properties;
@@ -14,15 +16,20 @@ public class NewOrderMain {
 
     public static void main(String args[]) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String,String>(properties());
-        var value = "1223112,888123,1289999";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",value, value);
-        producer.send(record,(data,ex)-> {
+        var key = UUID.randomUUID().toString();
+        var value = key + "1223112,888123,1289999";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",key, value);
+        Callback callback = (data, ex)-> {
             if(ex !=null){
                 ex.printStackTrace();
                 return;
             }
             System.out.println("Sucesso: " + data.topic() + ":::partition" + data.partition() + "/offset " + data.offset());
-        }).get();
+        };
+        var email = "Thank you for your order! We are processing your order.";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",email,email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord,callback).get();
     }
 
     public static Properties properties(){
