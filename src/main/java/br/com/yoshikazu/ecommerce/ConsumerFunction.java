3098aca9635dbf1136300ea3e6e193e1d3a4254c
yoshikzu <marcelo_yoshikazu@hotmail.com>
2023-09-05 20:05:54 -0300

    Criação das classes KafkaDispatcher e KafkaService e refatoração com as classes genéricas para producer e consumer

---
+++ src/main/java/br/com/yoshikazu/ecommerce/ConsumerFunction.java
@@ -0,0 +1,7 @@
+package br.com.yoshikazu.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface ConsumerFunction {
+    void consume(ConsumerRecord<String,String> record);
+}
--- src/main/java/br/com/yoshikazu/ecommerce/EmailService.java
+++ src/main/java/br/com/yoshikazu/ecommerce/EmailService.java
@@ -1,6 +1,7 @@
 package br.com.yoshikazu.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -11,32 +12,23 @@ import java.util.Properties;
 public class EmailService {
 
     public static void main(String args[]) throws InterruptedException {
-        var consumer = new KafkaConsumer<String,String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
-        var records = consumer.poll(Duration.ofMillis(100));
-        while(true){
-            for(var record: records){
-                System.out.println("----------------------------");
-                System.out.println("Send email!");
-                System.out.println(record.key());
-                System.out.println(record.value());
-                System.out.println(record.partition());
-                System.out.println(record.offset());
-                try{
-                    Thread.sleep(1000);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
+        var emailService = new EmailService();
+        try(var service = new KafkaService(EmailService.class.getSimpleName(),"ECOMMERCE_SEND_EMAIL", emailService::parse)){
+            service.run();
         }
     }
 
-    private static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-        return properties;
+    private void parse(ConsumerRecord<String,String> record){
+        System.out.println("----------------------------");
+        System.out.println("Send email!");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
